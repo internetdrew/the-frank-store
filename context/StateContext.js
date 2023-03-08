4dceb2183892d090939da5e5540d8ad0fa9b6d0f
internetdrew <andrew@internetdrew.com>
2023-03-07 22:35:26 -0500

    create context for global state management

---
+++ context/StateContext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+
+const Context = createContext();
+
+export const stateContext = ({ children }) => {
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQty, setTotalQty] = useState(0);
+  const [qty, setQty] = useState(1);
+  return (
+    <Context.Provider value={(cartItems, totalPrice, totalQty, qty)}>
+      {children}
+    </Context.Provider>
+  );
+};
