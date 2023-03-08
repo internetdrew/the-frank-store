222e281f0f85cc9fcb9b66caba8cce7f2dff7ef3
internetdrew <andrew@internetdrew.com>
2023-03-07 19:10:58 -0500

    render dynamic frank route pages

--- components/Franks.jsx
+++ components/Franks.jsx
@@ -3,7 +3,10 @@ import FrankCard from './FrankCard';
 const Franks = ({ franks }) => {
   return (
     <>
-      <section className='p-4 sm:p-28 mt-20'>
+      <section className='p-4 sm:p-28 mt-20 lg:mt-0'>
+        <h1 className='text-center text-4xl font-bold mb-12 lg:mb-20 text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-red-600'>
+          Shop your favorite Frank Costanza moments
+        </h1>
         <div className='grid grid-cols-1 gap-16 gap-y-36 md:grid-cols-3 w-full'>
           {franks.map(frank => (
             <FrankCard key={frank._id} frank={frank} />
--- pages/franks/[slug].js
+++ pages/franks/[slug].js
@@ -2,42 +2,7 @@ import { useState } from 'react';
 import { client, urlFor } from '@/lib/sanityClient';
 import Image from 'next/image';
 import { RageMeter } from '@/components';
-
-const FrankDetails = ({ frank }) => {
-  const { description, season, episode, image, quote, price, rage, title } =
-    frank;
-
-  return (
-    <section className='p-4 lg:p-32 mt-32 lg:mt-10'>
-      <div className='grid sm:grid-cols-2 gap-14 p-4'>
-        <Image
-          src={urlFor(image).url()}
-          alt={`image for ${title} frank`}
-          className='object-cover h-full rounded-3xl overflow-hidden w-full shadow-2xl'
-          width={900}
-          height={900}
-          priority
-        />
-        <div className='flex flex-col'>
-          <h1 className='text-5xl'>
-            <strong>{title}</strong> Frank
-          </h1>
-          <div className='flex items-center text-xl mt-2'>
-            <RageMeter rageLevel={rage} />
-          </div>
-          <p className='text-2xl mt-4'>{description}</p>
-          <div className='flex justify-center'>
-            <button className='w-full mt-10 bg-gradient-to-r from-yellow-500 to-red-600 py-4 rounded-full text-white font-semibold text-xl'>
-              Add to Cart
-            </button>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
-
-export default FrankDetails;
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 export const getStaticPaths = async () => {
   const query = `*[_type == "frank"]`;
@@ -64,3 +29,70 @@ export const getStaticProps = async ({ params: { slug } }) => {
     },
   };
 };
+
+const FrankDetails = ({ frank }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const { description, season, episode, image, quote, price, rage, title } =
+    frank;
+
+  return (
+    <section className='p-4 lg:p-32 mt-32 lg:mt-10'>
+      <div className='grid sm:grid-cols-2 gap-14 p-4'>
+        <Image
+          src={urlFor(image).url()}
+          alt={`image for ${title} frank`}
+          className='object-cover h-full rounded-3xl overflow-hidden w-full shadow-2xl'
+          width={300}
+          height={300}
+          priority
+        />
+        <div className='flex flex-col'>
+          <h1 className='text-5xl'>
+            <strong>{title}</strong> Frank
+          </h1>
+          <small>
+            Season {season}, Episode {episode}
+          </small>
+          <div className='flex items-center text-xl mt-1'>
+            <RageMeter franksRageLevel={rage} />
+          </div>
+          <h2 className='mt-8 text-3xl font-semibold'>Details</h2>
+          <p className='text-2xl'>{description}</p>
+          <h2 className='text-2xl font-bold text-orange-600 mt-4'>${price}</h2>
+
+          {/* Quantity */}
+          <div className='flex items-center justify-between mt-auto'>
+            <h2 className='text-2xl font-semibold'>Quantity:</h2>
+            <div className='flex items-center ml-10 justify-between gap-6 text-xl border py-2 px-8 rounded-full font-bold'>
+              <button
+                className='text-red-500 border-r pr-4'
+                onClick={() =>
+                  setQuantity(prevQty => {
+                    if (prevQty - 1 < 1) return 1;
+                    return prevQty - 1;
+                  })
+                }
+              >
+                <AiOutlineMinus />
+              </button>
+              <p className='w-8 text-center'>{quantity}</p>
+              <button
+                className='text-green-700 border-l pl-4'
+                onClick={() => setQuantity(prevQty => prevQty + 1)}
+              >
+                <AiOutlinePlus />
+              </button>
+            </div>
+          </div>
+
+          <button className='w-full flex-2 bg-gradient-to-r from-yellow-500 to-red-600 py-4 rounded-full text-white font-semibold mt-10 text-xl'>
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FrankDetails;
