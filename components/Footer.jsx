bc7ac5b732809d7c8ff983ea20d14f5b93b840d5
internetdrew <andrew@internetdrew.com>
2023-03-13 11:05:52 -0400

    add styles to footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,14 @@
 const Footer = () => {
   return (
-    <div className='py-2 text-center'>
-      <p>
-        An <a href='https://internetdrew.com'>Internet Drew</a> Creation
-      </p>
+    <div className='py-4 text-center flex items-center gap-1 font-semibold'>
+      <p>Made with love by </p>
+      <a
+        href='https://internetdrew.com'
+        target={'_blank'}
+        className='text-lg text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 to-red-600 motion-safe:animate-pulse'
+      >
+        <strong>Internet Drew</strong>
+      </a>
     </div>
   );
 };
